6d84873147964980032d7a5cd1633478d46312a8
Paul Orlob <orlobpaul@gmail.com>
2020-12-09 23:40:33 +0100

    Refactored server creation

--- cmd/energy-api/main.go
+++ cmd/energy-api/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/orlopau/go-energy/pkg/meter"
+	"github.com/orlopau/go-energy/pkg/sunspec"
 	"github.com/orlopau/go-sma-api/internal/api"
 	"github.com/orlopau/go-sma-api/internal/config"
+	"github.com/orlopau/go-sma-api/internal/plant"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 	"log"
@@ -37,17 +41,58 @@ func start() error {
 		return errors.Wrap(err, "error reading plants config")
 	}
 
-	log.Println("setting up server and SunSpec devices...")
-	server, err := api.NewServer(slaveId, confPlants)
+	log.Println("setting up energy devices")
+	plants, err := createPlants(slaveId, confPlants)
+	if err != nil {
+		return errors.Wrap(err, "error setting up plants")
+	}
+
+	log.Println("setting up server")
+	server, err := api.NewServer(plants)
 	if err != nil {
 		return err
 	}
 
-	log.Println("server started")
+	log.Println("server starting")
 	err = http.ListenAndServe(fmt.Sprintf(":%v", v.GetString(keyConfigPort)), server)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func createPlants(modbusSlaveId byte, plants map[string]config.Plant) (map[string]api.PlantFetcher, error) {
+	ps := make(map[string]api.PlantFetcher, len(plants))
+
+	meterListener, err := meter.Listen()
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range plants {
+		readers := make([]plant.PointReader, len(v.SunSpecAddrs))
+		for i, addr := range v.SunSpecAddrs {
+			ssr, err := sunspec.Connect(modbusSlaveId, addr)
+			if err != nil {
+				return nil, err
+			}
+			readers[i] = ssr
+		}
+
+		// TODO un-export GridMeter, add serial number filter (and therefore a ONE device energymeter) in go-energy
+		em := &plant.GridMeter{
+			EM:           meterListener,
+			SerialNumber: v.EnergyMeterSN,
+		}
+
+		p, err := plant.NewPlant(em, readers...)
+		if err != nil {
+			return nil, errors.Wrap(err, fmt.Sprintf("error creating plant %v", k))
+		}
+
+		ps[k] = plant.FetchContinuously(context.Background(), p)
+	}
+
+	return ps, nil
 }
--- internal/api/server.go
+++ internal/api/server.go
@@ -2,60 +2,25 @@ package api
 
 import (
 	"context"
-	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/orlopau/go-energy/pkg/meter"
-	"github.com/orlopau/go-energy/pkg/sunspec"
-	"github.com/orlopau/go-sma-api/internal/config"
 	"github.com/orlopau/go-sma-api/internal/plant"
-	"github.com/pkg/errors"
 	"net/http"
 )
 
-type plantFetcher interface {
+type PlantFetcher interface {
 	FetchSummary() (plant.Summary, error)
 }
 
 type server struct {
-	plants map[string]plantFetcher
+	plants map[string]PlantFetcher
 	router *mux.Router
 	ctx    context.Context
 }
 
-func NewServer(modbusSlaveId byte, plants map[string]config.Plant) (*server, error) {
-	ps := make(map[string]plantFetcher, len(plants))
-
-	meterListener, err := meter.Listen()
-	if err != nil {
-		return nil, err
-	}
-
-	for k, v := range plants {
-		readers := make([]plant.PointReader, len(v.SunSpecAddrs))
-		for i, addr := range v.SunSpecAddrs {
-			ssr, err := sunspec.Connect(modbusSlaveId, addr)
-			if err != nil {
-				return nil, err
-			}
-			readers[i] = ssr
-		}
-
-		em := &plant.GridMeter{
-			EM:           meterListener,
-			SerialNumber: v.EnergyMeterSN,
-		}
-
-		p, err := plant.NewPlant(em, readers...)
-		if err != nil {
-			return nil, errors.Wrap(err, fmt.Sprintf("error creating plant %v", k))
-		}
-
-		ps[k] = plant.FetchContinuously(context.TODO(), p)
-	}
-
+func NewServer(plants map[string]PlantFetcher) (*server, error) {
 	r := mux.NewRouter()
 	s := &server{
-		plants: ps,
+		plants: plants,
 		router: r,
 	}
 	s.routes()
